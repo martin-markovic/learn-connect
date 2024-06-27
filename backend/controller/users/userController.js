d5b819bd9e6667b3ff22b2610f6eced26b7702ca
Martin <martin730036@gmail.com>
2024-06-27 02:39:51 +0200

    update controller and connect db
    - implement `registerUser` and `loginUser` response and request validation.
    - implement `connectDB` by integrating `mongoose` in `db.js`.
    - integrate and instantiate `connectDB` into `server.js`.
    - refactor `server.test.js` by adding `describe` blocks with `before` to server initialization tests, and `before` and `after` for `socket.io` connection tests.

--- backend/controller/users/userController.js
+++ backend/controller/users/userController.js
@@ -1,12 +1,37 @@
 // POST users api/users/
-export const registerUser = (req, res) => {
-  res.status(201).json({
-    message: `User ${req.body.name} with an email ${req.body.email} was successfully registered`,
+export const registerUser = async (req, res) => {
+  const { name, email, password, password2 } = req.body;
+
+  if (!name || !email || !password || !password2) {
+    return res.status(400).json({ message: "Please add all fields" });
+  }
+
+  if (password !== password2) {
+    return res.status(400).json({ message: "Passwords must match" });
+  }
+
+  // const user = await User.findOne(email);
+
+  // if (!user) {
+  //   return res.status(201).json({
+  //     message: `User ${name} with an email ${email} was successfully registered`,
+  //   });
+  // }
+
+  // return res.status(400).json({ message: "User already registered" });
+
+  return res.status(201).json({
+    message: `User ${name} with an email ${email} was successfully registered`,
   });
 };
 
 // POST user api/users/login
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ message: "Please add all fields" });
+  }
+
   res.status(200).json({
     message: "User was successfully logged in",
   });
--- test/backend/server.test.js
+++ test/backend/server.test.js
@@ -1,24 +1,67 @@
 import { expect } from "chai";
 import { server, PORT } from "../../backend/server.js";
 import { io as clientIo } from "socket.io-client";
+import mongoose from "mongoose";
 
 describe("Server Tests", () => {
-  it("Server should start without errors", (done) => {
-    expect(server).to.not.be.undefined;
-    done();
-  });
+  describe("Server Initialization", () => {
+    let serverInstance;
 
-  it("Socket.io should handle connection and disconnection", (done) => {
-    const client = clientIo.connect(`http://localhost:${PORT}`);
-
-    client.on("connect", () => {
-      console.log("Client connected to socket.io");
-      client.disconnect();
+    before((done) => {
+      serverInstance = server;
+      done();
     });
 
-    client.on("disconnect", () => {
-      console.log("Client disconnected from socket.io");
+    it("should start without errors", (done) => {
+      expect(serverInstance).to.not.be.undefined;
       done();
+    });
+  });
+
+  describe("MongoDB Connection", () => {
+    before((done) => {
+      mongoose
+        .connect(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+        .then(() => done())
+        .catch((err) => done(err));
+    });
+
+    it("should connect to MongoDB successfully", (done) => {
+      mongoose.connection.once("open", () => {
+        expect(mongoose.connection.readyState).to.equal(1);
+        done();
+      });
+    });
+
+    after((done) => {
+      mongoose
+        .disconnect()
+        .then(() => done())
+        .catch((err) => done(err));
+    });
+  });
+
+  describe("Socket.io", () => {
+    let client;
+
+    before((done) => {
+      client = clientIo.connect(`http://localhost:${PORT}`);
+      client.on("connect", done);
+    });
+
+    it("should handle connction", (done) => {
+      expect(client.connected).to.be.true;
+      done();
+    });
+
+    after((done) => {
+      client.on("disconnect", () => {
+        done();
+      });
+      client.disconnect();
     });
   });
 });
