3af7eb0b5277cf720727e72c91396e8bd7e5b57e
Martin <martin730036@gmail.com>
2024-07-29 15:25:31 +0200

    refactor: Render list conditionally
    - integrate necessary hooks to conditionally render lists based on the component level state in `Quizzes.jsx`.
    - implement buttons for handling state change in `Quizzes.jsx`.
    - implement new classes layout  `quiz__list` and `quiz__list-controller` in `index.css`.
    - integrate layout classes into respective html `div` elements in `Quizze.jsx.`
    - integrate `toast` to handle errors in `QuizForm.jsx`.
    - change the html `label` element to `span` element in `ClassQuizList.jsx`.

--- frontend/src/pages/Quizzes.jsx
+++ frontend/src/pages/Quizzes.jsx
@@ -1,11 +1,52 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import ClassQuizList from "../components/ClassQuizList";
 import UserQuizList from "../components/UserQuizList";
 
 function Quizzes() {
+  const [listOpen, setListOpen] = useState(null);
+
+  useEffect(() => {
+    setListOpen(null);
+  }, []);
+
+  const { user } = useSelector((state) => state.auth);
+
+  const userName = user.name.split(" ")[0];
+
+  const handleClick = (e) => {
+    setListOpen(e.target.id);
+  };
+
   return (
-    <div>
-      <ClassQuizList />
-      <UserQuizList />
+    <div className="container quiz__list">
+      <div className="quiz__list-controller">
+        <button
+          type="button"
+          id="classroom"
+          onClick={handleClick}
+          disabled={listOpen === "classroom"}
+        >
+          Classroom quizzes
+        </button>
+        <button
+          type="button"
+          id="user"
+          onClick={handleClick}
+          disabled={listOpen === "user"}
+        >
+          {`${userName}'s`} quizzes
+        </button>
+      </div>
+      <div className="quiz__list-display">
+        {listOpen ? (
+          listOpen === "classroom" ? (
+            <ClassQuizList />
+          ) : (
+            <UserQuizList />
+          )
+        ) : null}
+      </div>
     </div>
   );
 }
