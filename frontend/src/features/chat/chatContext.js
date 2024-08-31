677dad3e7a87d26ce9a4961fe5b2a2c8fdd7a73a
Martin <martin730036@gmail.com>
2024-08-31 23:54:01 +0200

    refactor: chat context cleanup
    - remove chat messages, user classrooms, selected chat and activity state handling.
    - remove user selection from redux state.
    - remove classroom filtering based on the authenticated user.
    - clear useEffect's list of depenencies, and add.
    - remove `chatMessages`,  `selectedChat`, `setSelectedChat`, `classrooms`, `userClassrooms`, and `activity` from provider value list of properties.
    - add `socketInstance` and `displayAPI` to `contextValue`.
    - add socket initalization and connection to `useEffect`.
    - add toast notifications for successful socket connections and errors.
    
    Note: This commit aims to remove bloat, separate concerns and provide clear and maintainable `chatContext` code structure.

--- frontend/src/features/chat/chatContext.js
+++ frontend/src/features/chat/chatContext.js
@@ -1,48 +1,64 @@
-import { createContext, useContext, useState, useEffect, useRef } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import displayAPI from "../socket/displayHandlers.js";
+import configAPI from "../socket/configHandlers.js";
 import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const ChatContext = createContext();
 
-export const useChatContext = () => useContext(ChatContext);
-
 export const ChatProvider = ({ children }) => {
-  const [chatMessages, setChatMessages] = useState({});
-  const [userClassrooms, setUserClassrooms] = useState([]);
-  const [selectedChat, setSelectedChat] = useState(null);
-  const [activity, setActivity] = useState("");
-  const {
-    classrooms = [],
-    isLoading,
-    isError,
-  } = useSelector((state) => state.classroom);
-  const { user } = useSelector((state) => state.auth);
-
   const activityTimer = useRef(null);
+  const [socketInstance, setSocketInstance] = useState(null);
+  const { token } = useSelector((state) => state.auth.user);
 
   useEffect(() => {
-    if (!isError && !isLoading) {
-      const joinedClassrooms = classrooms.filter(
-        (classroom) =>
-          Array.isArray(classroom.students) &&
-          classroom.students.includes(user._id)
-      );
+    const initSocket = async () => {
+      if (token) {
+        // implement conditional API url based on process.env.mode
+        try {
+          const socket = await configAPI.handleConnect(
+            "http://localhost:8000",
+            token
+          );
 
-      setUserClassrooms(joinedClassrooms);
-    }
-  }, [classrooms, user._id, isLoading, isError]);
+          socket.on("connect", () => {
+            setSocketInstance(socket);
+          });
+
+          socket.on("connect_error", (err) => {
+            console.error("Connection failed:", err.message);
+            toast.error(
+              "Unable to connect to the server. Please try again later."
+            );
+          });
+        } catch (error) {
+          console.error("Socket connection returned null or undefined.");
+          toast.error(
+            "An unexpected error occurred. Please refresh the page or try again later."
+          );
+        }
+      } else {
+        console.error("User not authorized, no token.");
+      }
+    };
+
+    initSocket();
+
+    return () => {
+      if (socketInstance) {
+        configAPI.handleDisconnect(socketInstance);
+      }
+    };
+  }, [token]);
+
+  const contextValue = {
+    socketInstance,
+    ...displayAPI,
+  };
 
   return (
-    <ChatContext.Provider
-      value={{
-        chatMessages,
-        selectedChat,
-        setSelectedChat,
-        classrooms,
-        userClassrooms,
-        activity,
-      }}
-    >
-      {children}
-    </ChatContext.Provider>
+    <ChatContext.Provider value={contextValue}>{children}</ChatContext.Provider>
   );
 };
+
+export const useChatContext = () => useContext(ChatContext);
