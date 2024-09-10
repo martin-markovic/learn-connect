b7182dfd4364dc8b0fe3f916c78699e3f014cefa
Martin <martin730036@gmail.com>
2024-09-11 01:14:42 +0200

    refactor: refactor chat context
    - remove `ChatProvider` integration from `Chat.jsx`.
    - move `initSocket` function from chat context to `socketManager.js`.
    - integrate `useDispatch`, `initSocket` and `handleSocketEvents` into `chatContext.js`.
    - add `selectedChat` and `setSelectedChat` to contextValue.
    - integrate `ChatProvider` into `Dashboard.jsx`.
    
    - Note: the main focus of this commit is enabling components in `Dashboard.jsx` to utilize chat context. It also improves code organization, readbility, and separation of concerns of the `chatContext` module.

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,15 +1,12 @@
-import { ChatProvider } from "../../features/chat/chatContext.js";
 import ChatList from "./ChatList.jsx";
 import ChatDisplay from "./ChatDisplay.jsx";
 
 const Chat = () => {
   return (
-    <ChatProvider>
-      <div className="conversation">
-        <ChatList />
-        <ChatDisplay />
-      </div>
-    </ChatProvider>
+    <div className="conversation">
+      <ChatList />
+      <ChatDisplay />
+    </div>
   );
 };
 
--- frontend/src/features/chat/chatContext.js
+++ frontend/src/features/chat/chatContext.js
@@ -1,48 +1,20 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import displayAPI from "../socket/displayHandlers.js";
 import configAPI from "../socket/configHandlers.js";
-import { useSelector } from "react-redux";
-import { toast } from "react-toastify";
+import { initSocket, handleSocketEvents } from "../socket/socketManager.js";
 
 const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
-  const activityTimer = useRef(null);
   const [socketInstance, setSocketInstance] = useState(null);
+  const [selectedChat, setSelectedChat] = useState(null);
   const { token } = useSelector((state) => state.auth.user);
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const initSocket = async () => {
-      if (token) {
-        // implement conditional API url based on process.env.mode
-        try {
-          const socket = await configAPI.handleConnect(
-            "http://localhost:8000",
-            token
-          );
-
-          socket.on("connect", () => {
-            setSocketInstance(socket);
-          });
-
-          socket.on("connect_error", (err) => {
-            console.error("Connection failed:", err.message);
-            toast.error(
-              "Unable to connect to the server. Please try again later."
-            );
-          });
-        } catch (error) {
-          console.error("Socket connection returned null or undefined.");
-          toast.error(
-            "An unexpected error occurred. Please refresh the page or try again later."
-          );
-        }
-      } else {
-        console.error("User not authorized, no token.");
-      }
-    };
-
-    initSocket();
+    initSocket(token, setSocketInstance);
 
     return () => {
       if (socketInstance) {
@@ -51,8 +23,16 @@ export const ChatProvider = ({ children }) => {
     };
   }, [token]);
 
+  useEffect(() => {
+    if (socketInstance) {
+      handleSocketEvents(socketInstance, dispatch);
+    }
+  }, [socketInstance, dispatch]);
+
   const contextValue = {
     socketInstance,
+    selectedChat,
+    setSelectedChat,
     ...displayAPI,
   };
 
