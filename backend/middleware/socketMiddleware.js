91980ef073b19ca34ffb0582bc98db81abf21fad
Martin <martin730036@gmail.com>
2024-10-03 23:59:43 +0200

    refactor:
    - change argument name from `data` to `rooms` in `join room` event` in `handleRoomEvents` function in `roomController.js`.
    - change looping over room logic based on the rooms being an object in `roomController.js`.
    -  change looping over rooms logic based on the rooms being an object in `leave room` event in
     `roomController.js`.

--- backend/middleware/socketMiddleware.js
+++ backend/middleware/socketMiddleware.js
@@ -4,15 +4,20 @@ const socketMiddleware = (socket, next) => {
   const token = socket.handshake.auth?.token;
 
   if (!token) {
+    console.log("Token is missing from handshake auth");
     return next(new Error("Authentication error: Token is missing"));
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    socket.user = decoded;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decodedToken);
+    socket.user = { _id: decodedToken._id };
+
     next();
   } catch (error) {
-    return next(new Error("Authentication error: Invalid token"));
+    console.log("Invalid token:", error.message);
+    return next(new Error(`Authentication error: ${error.message}`));
   }
 };
 
