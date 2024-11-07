35299e586743bc466b82342339834fc14e66afa1
Martin <martin730036@gmail.com>
2024-11-08 00:44:39 +0100

    refactor: Chat message data handling
    - refactor `sender` field to consist of `id`  and `name` fields in `chatModel.js`.
    - move data validation to outer `handleSubmit` scope, to avoid emitting invalid data in `ChatDisplay.jsx.`
    - add required `messageData` and `clientData` fields to match the required chat schema and socket structure in `ChatDisplay.jsx`.
    - implement conditionally emitting valid `notificationData` on successful message submission in `ChatDisplay.jsx`.
    - remove the redundant async/await keywords from `handleRemove` function in `ChatDisplay.jsx`.
    
    - Note: This commit aims to improve data integrity and synchronization of client and server for chat message structure.

--- frontend/src/components/chat/ChatDisplay.jsx
+++ frontend/src/components/chat/ChatDisplay.jsx
@@ -59,13 +59,34 @@ const ChatDisplay = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!selectedChat && socketInstance && classroomId) {
+      console.error("Please select a chat and provide valid socket instance.");
+      return;
+    }
+
     try {
-      if (selectedChat && socketInstance && classroomId) {
-        const messageData = {
-          sender: user._id,
-          text: input,
+      const messageData = {
+        sender: { id: user?._id, name: user?.name },
+        text: input,
+        classroom: classroomId,
+        status: "pending",
+      };
+
+      const clientData = {
+        socketInstance,
+        eventName: "message room",
+        roomData: messageData,
+      };
+
+      const response = await emitRoomEvent(clientData);
+
+      if (response.success) {
+        setInput("");
+
+        const notificationData = {
+          sender: { id: user?._id, name: user?.name },
           classroom: classroomId,
-          status: "pending",
+          eventName: "new message",
         };
 
         const userData = {
@@ -111,7 +132,7 @@ const ChatDisplay = () => {
     }
   };
 
-  const handleRemove = async () => {
+  const handleRemove = () => {
     if (selectedChat) {
       try {
         const messageIds = messages[classroomId].map((message) => message._id);
@@ -123,7 +144,7 @@ const ChatDisplay = () => {
 
         const chatData = { classroomId, messageIds };
 
-        const result = await dispatch(removeMessages(chatData));
+        const result = dispatch(removeMessages(chatData));
         if (result.type === "chat/removeMessages/fulfilled") {
           console.log("Messages removed successfully");
         }
