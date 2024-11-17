197a52261642b69a5a1d2f70e8fe46195eb530dc
Martin <martin730036@gmail.com>
2024-11-17 23:56:19 +0100

    update: Implement server side friend controller
    - implement `sendFriendRequest` controller to create a new friend request and handle validation checks in `friendController.js`.
    - implement `handleFriendRequest` controller to accept, decline, or update the status of a friend request in `friendController.js`.
    - implement `getFriendList` controller to fetch and return a user's friend list in adjacency list format in `friendController.js`.
    - implement `getUserList` controller to retrieve and return a list of users excluding sensitive information like password and email in `friendController.js`.
    - create mock `removeFriend` controller, as a placeholder for future functionality to remove a friend in `friendController.js`.

---
+++ backend/controller/users/friendController.js
@@ -0,0 +1,133 @@
+import Friend from "../../models/users/friendModel.js";
+import User from "../../models/users/userModel.js";
+
+export const sendFriendRequest = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!userId) {
+      return res.status(400).json({ message: "Incorrect path" });
+    }
+
+    const senderId = req.user?._id;
+
+    if (!senderId) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+
+    const existingRequest = await Friend.findOne({
+      $or: [
+        { sender: senderId, receiver: userId, status: "pending" },
+        { sender: userId, receiver: senderId, status: "pending" },
+      ],
+    });
+
+    if (existingRequest) {
+      return res
+        .status(401)
+        .json({ message: "Friend request already pending" });
+    }
+
+    const newRequest = new Friend({
+      sender: senderId,
+      receiver: userId,
+      status: "pending",
+    });
+
+    await newRequest.save();
+
+    return res.status(200).json(newRequest);
+  } catch (error) {
+    console.error("Error processing friend request", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const handleFriendRequest = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const { friendReqResponse } = req.body;
+
+    const validStatuses = ["pending", "accepted", "declined"];
+
+    if (!validStatuses.includes(friendReqResponse)) {
+      return res.status(400).json({ message: "Invalid friend request status" });
+    }
+
+    if (friendReqResponse === "declined") {
+      await Friend.deleteOne({ receiver: userId, status: "pending" });
+      return res.status(200).json({ message: "Request declined" });
+    }
+
+    const friendRequest = await Friend.findOneAndUpdate(
+      { receiver: userId, status: "pending" },
+      { status: friendReqResponse },
+      { new: true }
+    );
+
+    if (!friendRequest) {
+      return res.status(404).json({ message: "Friend request not found" });
+    }
+
+    return res.status(200).json(friendRequest);
+  } catch (error) {
+    console.error("Error processing friend request", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getFriendList = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "User not authenticated" });
+    }
+
+    const friendList = await Friend.find({
+      $or: [{ sender: userId }, { receiver: userId }],
+    });
+
+    if (!friendList || friendList.length === 0) {
+      return res.status(200).json({});
+    }
+
+    const adjacencyList = friendList.reduce((acc, friend) => {
+      const { sender, receiver } = friend;
+      if (!acc[sender]) acc[sender] = [];
+      if (!acc[receiver]) acc[receiver] = [];
+      acc[sender].push(receiver);
+      acc[receiver].push(sender);
+      return acc;
+    }, {});
+
+    return res.status(200).json(adjacencyList);
+  } catch (error) {
+    console.error("Error fetching friend list", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getUserList = async (req, res) => {
+  try {
+    const userList = await User.find().select("-password -email -classrooms");
+
+    if (!userList || userList.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    return res.status(200).json(userList);
+  } catch (error) {
+    console.error("Error fetching list of users", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const removeFriend = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error fetching list of users", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
