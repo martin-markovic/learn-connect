ccb155e1e0853df74fe7f4c9b53bcb86792b8046
Martin <martin730036@gmail.com>
2024-11-18 23:59:58 +0100

    refactor: Friend slice cleanup
    - remove the redundant `sendFriendRequest`, and `handleFriendRequest` async thunks and their respective state handling scenarios in `friendSlice.js`.
    - move the logic from removed thunks into new actions to handle state updates in `friendSlice.js`

--- frontend/src/features/friend/friendSlice.js
+++ frontend/src/features/friend/friendSlice.js
@@ -11,45 +11,6 @@ const initialState = {
   userList: [],
 };
 
-export const sendFriendRequest = createAsyncThunk(
-  "friends/sendFriendRequest",
-  async (userId, thunkAPI) => {
-    try {
-      const token = thunkAPI.getState().auth.user?.token;
-      if (!token) {
-        throw new Error("Token not found");
-      }
-
-      const response = await friendService.sendFriendRequest(userId, token);
-
-      return response;
-    } catch (error) {
-      handleSliceError(error);
-    }
-  }
-);
-
-export const handleFriendRequest = createAsyncThunk(
-  "friends/handleFriendRequest",
-  async (clientData, thunkAPI) => {
-    try {
-      const token = thunkAPI.getState().auth.user?.token;
-      if (!token) {
-        throw new Error("Token not found");
-      }
-
-      const response = await friendService.handleFriendRequest(
-        clientData,
-        token
-      );
-
-      return response;
-    } catch (error) {
-      handleSliceError(error);
-    }
-  }
-);
-
 export const getFriendList = createAsyncThunk(
   "friends/getFriendList",
   async (_, thunkAPI) => {
@@ -87,51 +48,50 @@ export const getUserList = createAsyncThunk(
   }
 );
 
-export const removeFriend = createAsyncThunk(
-  "friends/removeFriend",
-  async (friendId, thunkAPI) => {
-    try {
-      const token = thunkAPI.getState().auth.user?.token;
-      if (!token) {
-        throw new Error("Token not found");
-      }
-
-      const response = await friendService.removeFriend(friendId, token);
-
-      return response;
-    } catch (error) {
-      handleSliceError(error);
-    }
-  }
-);
-
 const friendSlice = createSlice({
   name: "friends",
   initialState,
   reducers: {
     resetUserList: (state) => initialState,
+    sendFriendRequest: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.errorMessage = "";
+      if (!state.friendList[action.payload.sender]) {
+        state.friendList[action.payload.sender] = [];
+      }
+      if (!state.friendList[action.payload.receiver]) {
+        state.friendList[action.payload.receiver] = [];
+      }
+      state.friendList[action.payload.sender].push(action.payload.receiver);
+      state.friendList[action.payload.receiver].push(action.payload.sender);
+    },
+    handleFriendRequest: (state, action) => {
+      const { receiver, friendReqStatus } = action.payload;
+
+      if (friendReqStatus === "accepted") {
+        if (!state.friendList[receiver]) {
+          state.friendList[receiver] = [];
+        }
+        if (!state.friendList[action.payload.sender]) {
+          state.friendList[action.payload.sender] = [];
+        }
+        state.friendList[receiver].push(action.payload.sender);
+        state.friendList[action.payload.sender].push(receiver);
+      } else {
+        state.friendList[receiver] = state.friendList[receiver]?.filter(
+          (friend) => friend !== action.payload.sender
+        );
+        state.friendList[action.payload.sender] = state.friendList[
+          action.payload.sender
+        ]?.filter((friend) => friend !== receiver);
+      }
+    },
+    removeFriend: (state, action) => {},
   },
   extraReducers: (builder) => {
     builder
-      .addCase(sendFriendRequest.pending, (state) => {
-        state.isLoading = true;
-        state.isError = false;
-        state.errorMessage = "";
-      })
-      .addCase(sendFriendRequest.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = true;
-        state.isError = false;
-        state.errorMessage = "";
-        state.friendList[action.payload.receiverId] = action.payload.status;
-      })
-      .addCase(sendFriendRequest.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = false;
-        state.isError = true;
-        state.errorMessage =
-          action.payload?.message || "Failed to submit a friend request";
-      })
       .addCase(getFriendList.pending, (state) => {
         state.isLoading = true;
         state.isError = false;
