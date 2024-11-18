f630d6d7d7c460ed84e0062c272e00aed8932171
Martin <martin730036@gmail.com>
2024-11-18 23:58:04 +0100

    refactor: Chat model and add conversation model
    - add `participants` field with `sender` and `receiver` fields and remove `text` field in`chatSchema` in `chatModel.js`.
    - implement new Conversation schema with fields `sender`, `receiver`, `text`, `isRead` and `timestamp` to handle individual conversation storage in `conversationModel.js`.
    - add new `conversation` field as a reference to Conversation schema in `chatModel.js`.

---
+++ backend/models/chat/conversationModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
