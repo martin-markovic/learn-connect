bf373d6a56faf5c422008ab30684157f142cd5fe
Martin <martin730036@gmail.com>
2025-01-19 16:36:55 +0100

    refactor: Quiz error handling and route update
    - change controller function name instances from `getAllQuizzes` to `getUserQuizzes` for clarity and readability in `quizController.js` and `quizRoutes.js`.
    - implement logging descriptive error message string to the console for readability and clarity in `catch` blocks of the controller functions in `quizController.js`.

--- backend/controller/quizzes/quizController.js
+++ backend/controller/quizzes/quizController.js
@@ -2,7 +2,7 @@ import Classroom from "../../models/classrooms/classroomModel.js";
 import Quiz from "../../models/quizzes/quizModel.js";
 import User from "../../models/users/userModel.js";
 
-export const getAllQuizzes = async (req, res) => {
+export const getUserQuizzes = async (req, res) => {
   try {
     const userId = req.user._id;
 
@@ -16,8 +16,12 @@ export const getAllQuizzes = async (req, res) => {
 
     return res.status(200).json(quizzes || []);
   } catch (error) {
+    console.error(
+      `Error fetching quizzes for ${req.user._id}:  ${error.message}`
+    );
+
     return res.status(500).json({
-      message: `${error.message}`,
+      message: `Error fetching quizzes for ${req.user._id}: ${error.message}`,
     });
   }
 };
@@ -42,8 +46,10 @@ export const getQuiz = async (req, res) => {
 
     return res.status(200).json(quizFound);
   } catch (error) {
+    console.error(`Error fetching quiz ${req.params.id}: ${error.message}`);
+
     return res.status(500).json({
-      message: `${error.message}`,
+      message: `Error fetching quiz ${req.params.id}: ${error.message}`,
     });
   }
 };
@@ -81,7 +87,7 @@ export const getQuizzesByClassroom = async (req, res) => {
     return res.json(quizzes || []);
   } catch (error) {
     console.error("Error in getQuizzesByClassroom:", error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: `Server error: ${error.message}` });
   }
 };
 
@@ -113,8 +119,10 @@ export const updateQuiz = async (req, res) => {
 
     return res.status(200).json(updatedQuiz);
   } catch (error) {
+    console.error(`Error updating quiz ${req.params.id}: ${error.message}`);
+
     return res.status(500).json({
-      message: `${error.message}`,
+      message: `Error updating quiz ${req.params.id}: ${error.message}`,
     });
   }
 };
@@ -144,8 +152,10 @@ export const deleteQuiz = async (req, res) => {
       _id: req.params.id,
     });
   } catch (error) {
+    console.error(`Error deleting quiz ${req.params.id}: ${error.message}`);
+
     return res.status(500).json({
-      message: `${error.message}`,
+      message: `Error deleting quiz ${req.params.id}: ${error.message}`,
     });
   }
 };
