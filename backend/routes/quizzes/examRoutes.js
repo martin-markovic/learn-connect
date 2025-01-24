ec1ab6a36fcf9f484e6013a5a648fa0ea14c0e23
Martin <martin730036@gmail.com>
2025-01-24 23:54:48 +0100

    update: Exam feedback feature update
    - add new field `examFeedback` to score schema in `scoreModel.js`.
    - implement `getExamFeedback` function to handle client-passed data validation, user authentication, and valid `Exam` document fetching in `examController.js`.
    - integrate `getExamFeedback` in new API route endpoint `/:examId` in `examRoutes.js`.

--- backend/routes/quizzes/examRoutes.js
+++ backend/routes/quizzes/examRoutes.js
@@ -4,10 +4,12 @@ const examRoutes = Router();
 import {
   createExam,
   getExam,
+  getExamFeedback,
 } from "../../controller/quizzes/examController.js";
 import { protect } from "../../middleware/authMiddleware.js";
 
 examRoutes.post("/", protect, createExam);
 examRoutes.get("/", protect, getExam);
+examRoutes.get("/:examId", protect, getExamFeedback);
 
 export default examRoutes;
