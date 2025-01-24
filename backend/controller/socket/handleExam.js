f2681284801aaa896de156a0df476d5f4c5fec3d
Martin <martin730036@gmail.com>
2025-01-24 23:45:13 +0100

    update: Implement exam finish logic
    - implement `finishExam` to handle client-passed data validation, user authentication, error handling, `Exam` and `Score` document updates with the exam data, and `exam finished` socket event emission with score payload in `socket.exam.js`.
    - integrate `updateExam` in `finish exam` socket event listener in `handleExamEvents.js`.

--- backend/controller/socket/handleExam.js
+++ backend/controller/socket/handleExam.js
@@ -1,8 +1,13 @@
-import { updateExam } from "./helpers/socket.exam.js";
+import { updateExam, finishExam } from "./helpers/socket.exam.js";
 
 const handleExamEvents = (context) => {
   context.socket.on("update exam", async (data) => {
     await updateExam(context, data);
+  });
+
+  context.socket.on("finish exam", async (data) => {
+    await finishExam(context, data);
+  });
 };
 
 export default handleExamEvents;
--- backend/controller/socket/helpers/socket.exam.js
+++ backend/controller/socket/helpers/socket.exam.js
@@ -44,3 +44,82 @@ export const updateExam = async (context, data) => {
   }
 };
 
+export const finishExam = async (context, data) => {
+  try {
+    const { senderId, receiverId } = data;
+
+    const examFound = await Exam.findOne({ studentId: senderId });
+
+    if (!examFound) {
+      throw new Error("Exam not found");
+    }
+
+    const examIsValid = examFound?.examFinish.getTime() - Date.now();
+
+    if (!examIsValid) {
+      throw new Error("Exam has expired");
+    }
+
+    const quizFound = await Quiz.findOne({ quizId: receiverId });
+
+    if (!quizFound) {
+      throw new Error("Cannot evaluate exam, no matching quiz found");
+    }
+
+    let payloadScore = 0;
+
+    const examFeedback = [];
+
+    for (let i = 0; i < quizFound.questions.length; i++) {
+      const q = quizFound.questions[i];
+
+      if (q.answer === examFound.answers[i]) {
+        payloadScore += 1;
+      }
+
+      examFeedback[i] = {
+        userAnswer: examFound.answers[i],
+        correctAnswer: q.answer,
+      };
+    }
+
+    const scoreFound = await Score.findOne({
+      user: senderId,
+      quiz: quizFound?._id,
+    });
+
+    let scorePayload;
+
+    if (scoreFound) {
+      scorePayload = await Score.findByIdAndUpdate(
+        scoreFound?._id,
+        {
+          $set: {
+            examFeedback,
+            highScore: Math.max(scoreFound.highScore, payloadScore),
+          },
+        },
+        { new: true }
+      );
+    } else {
+      const newScore = new Score({
+        user: senderId,
+        quiz: quizFound?._id,
+        examFeedback,
+        highScore: payloadScore,
+      });
+
+      payloadScore = await newScore.save();
+    }
+
+    context.emitEvent("sender", "exam finished", scorePayload);
+  } catch (error) {
+    console.error(
+      `Error finishing exam ${data?.receiverId}:  ${error.message}`
+    );
+
+    context.emitEvent("sender", "error", {
+      message: `Error finishing exam ${data?.receiverId}:  ${error.message}`,
+    });
+  }
+};
