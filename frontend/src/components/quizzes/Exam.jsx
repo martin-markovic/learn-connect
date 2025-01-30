0f920d48759a9aa2998f746c857c0c6c9bfe41ec
Martin <martin730036@gmail.com>
2025-01-30 23:56:43 +0100

    update: Integrate socket features in Exam.jsx
    - integrate `useSelector` to handle providing authenticated user id as `senderId` for socket related events in `Exam.jsx`.
    - integrate `useNavigate` to handle navigating to quiz page on exam finish in `Exam.jsx`.
    -integrate `socketEventManager` to handle event emission and subscription to exam-related socket events in `Exam.jsx.`
    - implement tracking current question and state with `useState` to handle rendering individual exam questions in `Exam.jsx`.
    - integrate dispatching `updateExam` redux action with event passed data on `exam updated` socket event in `Exam.jsx`.
    - integrate dispatching `finishExam` redux action with event passed data and navigating to the payload quiz page on `exam finished` socket event in `Exam.jsx`.
    - implement unsubscribing to `exam updated ` and `exam finished` socket events on component unmount in `Exam.jsx`.
    - implement `handleChange` to handle emitting `update exam` socket event with the updated exam and user data in `Exam.jsx`.
    - implement `handleChange` to handle emitting socket event `finish exam` with user data via in `Exam.jsx`.

--- frontend/src/components/quizzes/Exam.jsx
+++ frontend/src/components/quizzes/Exam.jsx
@@ -1,16 +1,77 @@
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getExam, updateExam } from "../../features/quizzes/exam/examSlice.js";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getExam,
+  updateExam,
+  finishExam,
+  resetExam,
+} from "../../features/quizzes/exam/examSlice.js";
+import socketEventManager from "../../features/socket/socket.eventManager.js";
 
 function Exam() {
-  const { examData, setExamData } = useState(null);
+  const [currQuestion, setCurrQuestion] = useState(0);
+  const { isLoading, examData } = useSelector((state) => state.exam);
+  const { user } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getExam());
+
+    return () => {
+      dispatch(resetExam());
+    };
+  }, [dispatch]);
+
 
   useEffect(() => {
     if (!examData) {
       dispatch(getExam());
+  useEffect(() => {
+    socketEventManager.subscribe("exam updated", (data) => {
+      dispatch(updateExam(data));
+    });
+
+    socketEventManager.subscribe("exam finished", (data) => {
+      dispatch(finishExam(data));
+
+      navigate(`/quizzes/${data?.scorePayload?.quiz}`);
+    });
+
+    return () => {
+      socketEventManager.unsubscribe("exam updated");
+      socketEventManager.unsubscribe("exam finished");
+    };
+  }, [dispatch, navigate]);
+
+  const handleChange = (e) => {
+    try {
+      const userData = {
+        senderId: user?._id,
+        receiverId: examData?._id,
+        examData: {
+          choiceIndex: currQuestion,
+          choiceData: e.target.id,
+        },
+      };
+
+      socketEventManager.handleEmitEvent("update exam", userData);
+    } catch (error) {
+      console.error("Error selecting option: ", error.message);
     }
-  }, [examData, dispatch]);
+  };
+
+  const handleSubmit = () => {
+    try {
+      socketEventManager.handleEmitEvent("finish exam", {
+        senderId: user?._id,
+      });
+    } catch (error) {
+      console.error("Error finishing exam: ", error.message);
+    }
+  };
 
 
   return <div>{examData ? <p>ExamData</p> : <p>No exam</p>}</div>;
