1917e6c532d865c42b229740ee001534f1b419c5
Martin <martin730036@gmail.com>
2025-01-30 23:59:52 +0100

    update: Exam.jsx rendering and navigation
    - implement tracking `errorMessage` state with `useState` to handle exam data validation, user authentication, exam expiration date, and re-routing ti `/quizzes` in `Exam.jsx`.
    - implement conditionally rendering exam data in `Exam.jsx`.

--- frontend/src/components/quizzes/Exam.jsx
+++ frontend/src/components/quizzes/Exam.jsx
@@ -11,6 +11,7 @@ import socketEventManager from "../../features/socket/socket.eventManager.js";
 
 function Exam() {
   const [currQuestion, setCurrQuestion] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(null);
   const { isLoading, examData } = useSelector((state) => state.exam);
   const { user } = useSelector((state) => state.auth);
 
@@ -25,10 +26,42 @@ function Exam() {
     };
   }, [dispatch]);
 
+  useEffect(() => {
+    if (!isLoading) {
+      if (!examData?._id) {
+        setErrorMessage("No exam is currently in progress");
+      }
+
+      if (examData?._id && examData?.studentId !== user?._id) {
+        setErrorMessage("User id does not match this exam student id");
+      }
+
+      const examExpireDate = new Date(examData?.examFinish).getTime();
+
+      const isExamValid = examExpireDate - Date.now() > 0;
+
+      if (examData?._id && !isExamValid) {
+        setErrorMessage("Exam has expired");
+      }
+    }
+  }, [
+    isLoading,
+    navigate,
+    examData?._id,
+    examData?.studentId,
+    user?._id,
+    errorMessage,
+    examData?.examFinish,
+  ]);
 
   useEffect(() => {
-    if (!examData) {
-      dispatch(getExam());
+    if (errorMessage) {
+      console.error(errorMessage);
+      setErrorMessage(null);
+      navigate("/quizzes");
+    }
+  }, [errorMessage, navigate]);
+
   useEffect(() => {
     socketEventManager.subscribe("exam updated", (data) => {
       dispatch(updateExam(data));
@@ -73,8 +106,68 @@ function Exam() {
     }
   };
 
+  return (
+    <div>
+      {examData ? (
+        <div>
+          {examData?.shuffledQuestions?.length &&
+            examData?.shuffledQuestions.find(
+              (_, index) => index === currQuestion
+            ) && (
+              <div>
+                <h1>{examData?.shuffledQuestions[currQuestion]?.question}</h1>
 
-  return <div>{examData ? <p>ExamData</p> : <p>No exam</p>}</div>;
+                <div>
+                  {examData?.shuffledQuestions[currQuestion]?.choices.map(
+                    (choice, choiceIndex) => {
+                      return (
+                        <div key={`choice-${choiceIndex}`}>
+                          <input
+                            name="choice"
+                            type="radio"
+                            id={choice}
+                            onChange={handleChange}
+                            checked={examData.answers[currQuestion] === choice}
+                          />
+                          <label htmlFor="choice" id={choice}>
+                            {choice}
+                          </label>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+                {currQuestion > 0 && (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setCurrQuestion((prevState) => prevState - 1);
+                    }}
+                  >
+                    Previous Question
+                  </button>
+                )}
+                {currQuestion < examData.shuffledQuestions.length - 1 && (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setCurrQuestion(() => currQuestion + 1);
+                    }}
+                  >
+                    Next Question
+                  </button>
+                )}
+                <button type="button" onClick={handleSubmit}>
+                  Finish Exam
+                </button>
+              </div>
+            )}
+        </div>
+      ) : (
+        <p>No exam</p>
+      )}
+    </div>
+  );
 }
 
 export default Exam;
