662158188603550aff93932d7f8c708c7c0c676a
Martin <martin730036@gmail.com>
2025-04-06 00:57:35 +0200

    refactor: Message creation for empty chat
    - change `chatFound` declaration from `const` to `let`, assign `chatFound` to newly saved `Chat` collection in `sendMessage` in `socket.messages.js`.
    - change `chatId` property value from `newChat._id` to `chatFound?._id.toString()` in `messagePayload object in `sendMessage` in `socket.messages.js`.
    - remove integration of `useCallback` hook in `ClassQuizList.jsx`, an update missed in previous commits.

--- frontend/src/components/quizzes/ClassQuizList.jsx
+++ frontend/src/components/quizzes/ClassQuizList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
