cb1cc00f33c3249615e0ba807005290f76bd744d
Martin <martin730036@gmail.com>
2025-05-16 21:40:33 +0200

    refactor: Server side user presence modularization
    - integrate `User` schema model to handle lookup and tracking `online` property,
    - remove `isOnline` and introduce `connectionStatus` parameter to allow more granular control over event emission to the client, based on the type of status change.
    - refactor friend list lookup into a helper function `getOnlineList`.
    - add sender and receiver id conversion to String before the comparison, and add `online` field to validation of online friends.
    - implement `currentStatus` tracking based on the User document `online` field, to return early from event emission and building online list logic, if user turned off chat events.

--- backend/controller/socket/helpers/socket.userPresence.js
+++ backend/controller/socket/helpers/socket.userPresence.js
@@ -1,50 +1,95 @@
 import { getUserSocketMap } from "./socket.init.js";
 import Friend from "../../../models/users/friendModel.js";
+import User from "../../../models/users/userModel.js";
 
-export const handleConnectionStatus = async (context, userData) => {
-  const { userId, isOnline } = userData;
+export const handleConnectionStatus = async (
+  context,
+  userId,
+  connectionStatus
+) => {
+  try {
+    if (!userId) {
+      throw new Error("User id not found");
+    }
 
+    const userFound = await User.findOne({ _id: userId });
+
+    if (!userFound) {
+      throw new Error("User is not authorized");
+    }
+
+    const currentStatus = userFound?.online;
+
+    if (
+      connectionStatus === "connected" ||
+      connectionStatus === "reconnected"
+    ) {
+      if (!currentStatus) return;
+
+      const payloadList = await getOnlineList(userId);
+
+      context.emitEvent("sender", "user connected", { payloadList });
+
+      payloadList.forEach((onlineUser) => {
+        context.emitEvent("receiver", "friend connected", {
+          id: userId,
+          receiverId: onlineUser,
+        });
+      });
+    }
+
+    if (connectionStatus === "disconnected" || connectionStatus === "offline") {
+      if (!currentStatus && connectionStatus === "disconnected") return;
+
+      const payloadList = await getOnlineList(userId);
+
+      payloadList.forEach((onlineUser) => {
+        context.emitEvent("receiver", "friend disconnected", {
+          id: userId,
+          receiverId: onlineUser,
+        });
+      });
+    }
+  } catch (error) {
+    console.error("Error changing user connection status: ", error.message);
+    context.emitEvent("sender", "error", { message: error.message });
+  }
+};
+
+const getOnlineList = async (userId) => {
   const friendList = await Friend.find(
     {
       $or: [{ sender: userId }, { receiver: userId }],
     },
     "sender receiver"
-  );
-
-  const userMap = getUserSocketMap();
+  )
+    .populate("sender", "online")
+    .populate("receiver", "online");
 
   const onlineList = new Set();
 
   friendList.forEach((entry) => {
-    const senderId = entry.sender.toString();
-    const receiverId = entry.receiver.toString();
+    const senderId = entry.sender._id.toString();
+    const receiverId = entry.receiver._id.toString();
 
-    if (userMap.has(senderId) && senderId !== userId.toString()) {
+    const userSocketMap = getUserSocketMap();
+
+    if (
+      entry.sender.online &&
+      senderId.toString() !== userId.toString() &&
+      userSocketMap.has(senderId)
+    ) {
       onlineList.add(senderId);
     }
 
-    if (userMap.has(receiverId) && receiverId !== userId.toString()) {
+    if (
+      entry.receiver.online &&
+      receiverId.toString() !== userId.toString() &&
+      userSocketMap.has(receiverId)
+    ) {
       onlineList.add(receiverId);
     }
   });
 
-  const payloadList = Array.from(onlineList);
-
-  if (isOnline) {
-    context.emitEvent("sender", "user connected", { payloadList });
-
-    payloadList.forEach((onlineUser) => {
-      context.emitEvent("receiver", "friend connected", {
-        id: userId,
-        receiverId: onlineUser,
-      });
-    });
-  } else {
-    payloadList.forEach((onlineUser) => {
-      context.emitEvent("receiver", "friend disconnected", {
-        id: userId,
-        receiverId: onlineUser,
-      });
-    });
-  }
+  return Array.from(onlineList);
 };
