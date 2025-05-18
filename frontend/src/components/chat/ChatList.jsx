9129f5eb11032126f2900992d1c765978bebcf9a
Martin <martin730036@gmail.com>
2025-05-18 14:40:55 +0200

    refactor: ChatList layout css updates
    - change `h3` element to `p` in `ChatList.jsx`.
    -  add friendlist map scope level variable `isFriend` to limit chat for accepted friend requests in `ChatList.jsx`.
    - add explicit return of `null` value when chat list entry status are still pending in `ChatList.jsx`.
    - implement css style properties `background`, `width`, `padding` and `font-weight` for `p` element within `chatlist-container` in `index.css`.
    - remove `justify-content` property from `.friendlist__container-wrapper` class in `index.css`.
    - add `margin-left` and remove `display` property in `.chatlist__user-status` class in `index.css`.

--- frontend/src/components/chat/ChatList.jsx
+++ frontend/src/components/chat/ChatList.jsx
@@ -63,37 +63,53 @@ function ChatList() {
             </p>
           ) : friendList && friendList.length > 0 ? (
             <div className="chatlist-container">
-              <h3>Your Friends:</h3>
+              <p>Your Friends:</p>
               <ul className="friendlist">
                 {friendList.map((entry, index) => {
                   const isOnline =
                     onlineList?.includes(entry.senderId) ||
                     onlineList?.includes(entry.receiverId);
 
-                  return (
-                    <li
-                      className="clickable"
-                      key={`chat-${index}`}
-                      onClick={() =>
-                        handleSelect(
-                          entry.senderId === user?._id
-                            ? {
-                                id: entry.receiverId,
-                                name: entry.receiverName,
-                                avatar: entry?.receiverAvatar,
-                              }
-                            : {
-                                id: entry.senderId,
-                                name: entry.senderName,
-                                avatar: entry.senderAvatar,
-                              }
-                        )
-                      }
-                    >
-                      <span className="chatlist__avatar-wrapper">
-                        {entry.senderId === user?._id ? (
-                          entry.receiverAvatar ? (
-                            <img src={entry.receiverAvatar} alt="avatar" />
+                  const isFriend = entry.status === "accepted";
+
+                  if (isFriend)
+                    return (
+                      <li
+                        className="clickable"
+                        key={`chat-${index}`}
+                        onClick={() =>
+                          handleSelect(
+                            entry.senderId === user?._id
+                              ? {
+                                  id: entry.receiverId,
+                                  name: entry.receiverName,
+                                  avatar: entry?.receiverAvatar,
+                                }
+                              : {
+                                  id: entry.senderId,
+                                  name: entry.senderName,
+                                  avatar: entry.senderAvatar,
+                                }
+                          )
+                        }
+                      >
+                        <span className="chatlist__avatar-wrapper">
+                          {entry.senderId === user?._id ? (
+                            entry.receiverAvatar ? (
+                              <img src={entry.receiverAvatar} alt="avatar" />
+                            ) : (
+                              <div className="chatlist__avatar__icon-container">
+                                <FaCircleUser
+                                  style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    color: "grey",
+                                  }}
+                                />
+                              </div>
+                            )
+                          ) : entry.senderAvatar ? (
+                            <img src={entry.senderAvatar} alt="avatar" />
                           ) : (
                             <div className="chatlist__avatar__icon-container">
                               <FaCircleUser
@@ -104,31 +120,20 @@ function ChatList() {
                                 }}
                               />
                             </div>
-                          )
-                        ) : entry.senderAvatar ? (
-                          <img src={entry.senderAvatar} alt="avatar" />
-                        ) : (
-                          <div className="chatlist__avatar__icon-container">
-                            <FaCircleUser
-                              style={{
-                                width: "100%",
-                                height: "100%",
-                                color: "grey",
-                              }}
-                            />
-                          </div>
+                          )}
+                        </span>
+                        <span>
+                          {entry.senderId === user?._id
+                            ? entry.receiverName
+                            : entry.senderName}
+                        </span>
+                        {isOnline && (
+                          <span className="chatlist__user-status"></span>
                         )}
-                      </span>
-                      <span>
-                        {entry.senderId === user?._id
-                          ? entry.receiverName
-                          : entry.senderName}
-                      </span>
-                      {isOnline && (
-                        <span className="chatlist__user-status"></span>
-                      )}
-                    </li>
-                  );
+                      </li>
+                    );
+
+                  return null;
                 })}
               </ul>
             </div>
