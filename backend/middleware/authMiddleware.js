8b6344b49e338a5b37d306b3393770b4ee15e89a
Martin <martin730036@gmail.com>
2025-07-14 23:31:02 +0200

    refactor: Error handling in auth middleware
    - refactor the `protect` middleware to provide robust error handling by splitting error messages into debugging and user-oriented categories, add clearer context for user-oriented error messages, and add fallback error status codes and messages for integrity.
    - implement a helper `decodeToken` function to handle `JWT` verification and robust error case handling, improving maintainability and clarity.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,37 +2,92 @@ import jwt from "jsonwebtoken";
 import User from "../models/users/userModel.js";
 
 export const protect = async (req, res, next) => {
-  let token;
+  try {
+    const authHeader = req.headers.authorization;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      console.error("Missing or invalid auth header");
 
-      if (!token) {
-        throw new Error("No token provided");
-      }
-
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-      if (!decodedToken || !decodedToken.id) {
-        throw new Error("Invalid token");
-      }
-
-      req.user = await User.findById(decodedToken?.id);
-
-      if (!req.user) {
-        throw new Error("User not found in the database");
-      }
-
-      next();
-    } catch (error) {
-      console.error("Error authorizing request: ", error.message);
-      return res.status(401).json({ message: error.message });
+      throw { status: 400, message: "Login required to access this resource." };
     }
-  } else {
-    return res.status(401).json({ message: "Not authorized" });
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      console.error("Token not provided");
+
+      throw { status: 400, message: "Authentication failed. Please relog." };
+    }
+
+    const { tokenValid, errorStatus, errorMessage } = decodeToken(token);
+
+    if (!tokenValid) {
+      throw {
+        status: errorStatus || 401,
+        message: errorMessage || "Session expired. Please log in.",
+      };
+    }
+
+    req.user = await User.findById(tokenValid?.id);
+
+    if (!req.user) {
+      console.error("User not found in the database");
+
+      throw { status: 401, message: "Your account could not be verified." };
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error authorizing request: ", error.message);
+
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || "Server error" });
+  }
+};
+
+const decodeToken = (token) => {
+  try {
+    const tokenValid = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!tokenValid || !tokenValid.id) {
+      console.error("Invalid token");
+
+      return {
+        tokenValid: null,
+        errorStatus: 400,
+        errorMessage: "Session expired. Please log in again.",
+      };
+    }
+
+    return { tokenValid };
+  } catch (error) {
+    console.error("JWT verification failed:", error.message);
+
+    if (error.message === "jwt malformed") {
+      return {
+        tokenValid: null,
+        errorStatus: 400,
+        errorMessage: "Malformed token. Please log in again.",
+      };
+    } else if (error.message === "invalid token") {
+      return {
+        tokenValid: null,
+        errorStatus: 401,
+        errorMessage: "Invalid token. Session not recognized.",
+      };
+    } else if (error.message === "jwt expired") {
+      return {
+        tokenValid: null,
+        errorStatus: 401,
+        errorMessage: "Session expired. Please log in again.",
+      };
+    }
+
+    return {
+      tokenValid: null,
+      errorStatus: error.status || 500,
+      errorMessage: error.message || "Authentication failed. Please log in.",
+    };
   }
 };
