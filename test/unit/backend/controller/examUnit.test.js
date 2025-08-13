a828361bbbc0d968d13f6128d70d0f29f751043d
Martin <martin730036@gmail.com>
2025-08-14 00:23:17 +0200

    update: Finish getExam test suite cases
    - add new global scope variable `expiredExamUser` and create the `User` doc using it in `before` code block.
    - change the `expiredExam` property `studentId` value to `expiredExamUser._id`, and `isInProgress` property value to `true`.
    - implement `getExam` successful case for users with no ongoing exams, unauthorized request, and the expired exam payload cases.

--- test/unit/backend/controller/examUnit.test.js
+++ test/unit/backend/controller/examUnit.test.js
@@ -23,6 +23,7 @@ const mockQuiz = mockExamData.mockQuizzes[0];
 
 const hasAnExamUser = mockExamData.mockUsers[0];
 const noExamsUser = mockExamData.mockUsers[1];
+const expiredExamUser = mockExamData.mockUsers[2];
 
 const ongoingExam = {
   _id: "1",
@@ -38,18 +39,19 @@ const ongoingExam = {
 const expiredExam = {
   _id: "2",
   quizId: mockQuiz._id,
-  studentId: hasAnExamUser._id,
+  studentId: expiredExamUser._id,
   examStart: new Date(Date.now() - 60 * 1000 * 10),
   examFinish: new Date(Date.now() - 60 * 1000 * 5),
   shuffledQuestions: [],
   answers: [],
-  isInProgress: false,
+  isInProgress: true,
 };
 
 describe("Exam API", () => {
   before(async () => {
     await MockUserModel.create(hasAnExamUser);
     await MockUserModel.create(noExamsUser);
+    await MockUserModel.create(expiredExamUser);
 
     await MockExamModel.create(ongoingExam);
     await MockExamModel.create(expiredExam);
@@ -75,6 +77,44 @@ describe("Exam API", () => {
       for (const [key, value] of Object.entries(examRes.body)) {
         expect(ongoingExam[key]).to.equal(value);
       }
+    });
+
+    it("should return an empty object for users without ongoing exams", async () => {
+      const mockReq = {
+        user: { _id: noExamsUser._id },
+      };
+
+      await mockGetExam(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(200);
+      expect(examRes.body).to.be.an("object").that.is.empty;
+    });
+
+    it("should return a `Error fetching exam: User id is required` error message", async () => {
+      const mockReq = {
+        user: { _id: undefined },
+      };
+
+      await mockGetExam(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(403);
+      expect(examRes.body.message).to.equal(
+        "Error fetching exam: User id is required"
+      );
+    });
+
+    it("should return an empty object as an expired exam payload", async () => {
+      const mockReq = {
+        user: { _id: expiredExamUser._id },
+      };
+
+      await mockGetExam(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(200);
+      expect(examRes.body).to.be.an("object").that.is.empty;
+
+      // mockGetExam deletes the stored expiredExam document
+      await MockExamModel.create(expiredExam);
     });
   });
 });
