a977cdfbc4b2ce082a3e6d251201da7e23bd5279
Martin <martin730036@gmail.com>
2025-08-14 22:49:08 +0200

    update: Add getExamFeedback test cases
    - implement monkey-patch clause as a solution for handling exam feedback query operations by filtering out score documents matching the quiz and user ID fields, in `findOne` method in `mockModel.js`.
    - integrate `mongoose` to handle setting `_id` property values for mock document global variables to `new mongoose.Types.ObjectId` in `examUnit.test.js`.
    - add new global variable `mockScore` for asserting exam feedback operations in `examUnit.test.js`.
    - convert instances of `mockReq` quiz and user id values to Strings to match in `examUnit.test.js`.
    - implement `getExamFeedback` test cases for successful, unauthenticated, missing quiz parameter, and non-existing exam API calls in `examUnit.test.js`.

--- test/unit/backend/controller/examUnit.test.js
+++ test/unit/backend/controller/examUnit.test.js
@@ -7,6 +7,7 @@ import {
   getExamFeedback,
   getExamScores,
 } from "../../../../backend/controller/quizzes/examController.js";
+import mongoose from "mongoose";
 
 const MockExamModel = new MockModel("exams");
 const MockScoreModel = new MockModel("scores");
@@ -19,15 +20,22 @@ const mockGetExamScores = getExamScores(MockScoreModel);
 const mockExamData = new MockData();
 const examRes = new MockRes();
 
-const mockQuiz = mockExamData.mockQuizzes[0];
-
-const hasAnExamUser = mockExamData.mockUsers[0];
-const noExamsUser = mockExamData.mockUsers[1];
-const expiredExamUser = mockExamData.mockUsers[2];
+const hasAnExamUser = {
+  ...mockExamData.mockUsers[0],
+  _id: new mongoose.Types.ObjectId(),
+};
+const noExamsUser = {
+  ...mockExamData.mockUsers[1],
+  _id: new mongoose.Types.ObjectId(),
+};
+const expiredExamUser = {
+  ...mockExamData.mockUsers[2],
+  _id: new mongoose.Types.ObjectId(),
+};
 
 const ongoingExam = {
-  _id: "1",
-  quizId: mockQuiz._id,
+  _id: new mongoose.Types.ObjectId(),
+  quizId: new mongoose.Types.ObjectId(),
   studentId: hasAnExamUser._id,
   examStart: new Date(Date.now()),
   examFinish: new Date(Date.now() + 60 * 1000 * 10),
@@ -37,14 +45,23 @@ const ongoingExam = {
 };
 
 const expiredExam = {
-  _id: "2",
-  quizId: mockQuiz._id,
+  _id: new mongoose.Types.ObjectId(),
+  quizId: new mongoose.Types.ObjectId(),
   studentId: expiredExamUser._id,
   examStart: new Date(Date.now() - 60 * 1000 * 10),
   examFinish: new Date(Date.now() - 60 * 1000 * 5),
   shuffledQuestions: [],
   answers: [],
   isInProgress: true,
+};
+
+const mockScore = {
+  _id: new mongoose.Types.ObjectId(),
+  user: expiredExamUser._id,
+  quiz: { quizId: expiredExam.quizId, quizTitle: "Mock Quiz Title" },
+  highScore: 1,
+  latestScore: 1,
+  examFeedback: {},
 };
 
 describe("Exam API", () => {
@@ -55,6 +72,8 @@ describe("Exam API", () => {
 
     await MockExamModel.create(ongoingExam);
     await MockExamModel.create(expiredExam);
+
+    await MockScoreModel.create(mockScore);
   });
 
   beforeEach(() => {
@@ -68,7 +87,7 @@ describe("Exam API", () => {
   describe("getExam", () => {
     it("should fetch an ongoing exam and verify it", async () => {
       const mockReq = {
-        user: { _id: hasAnExamUser._id },
+        user: { _id: hasAnExamUser._id.toString() },
       };
 
       await mockGetExam(mockReq, examRes);
@@ -81,7 +100,7 @@ describe("Exam API", () => {
 
     it("should return an empty object for users without ongoing exams", async () => {
       const mockReq = {
-        user: { _id: noExamsUser._id },
+        user: { _id: noExamsUser._id.toString() },
       };
 
       await mockGetExam(mockReq, examRes);
@@ -105,7 +124,7 @@ describe("Exam API", () => {
 
     it("should return an empty object as an expired exam payload", async () => {
       const mockReq = {
-        user: { _id: expiredExamUser._id },
+        user: { _id: expiredExamUser._id.toString() },
       };
 
       await mockGetExam(mockReq, examRes);
@@ -115,6 +134,63 @@ describe("Exam API", () => {
 
       // mockGetExam deletes the stored expiredExam document
       await MockExamModel.create(expiredExam);
+    });
+  });
+
+  describe("getExamFeedback", () => {
+    it("should fetch the exam feedback and verify it", async () => {
+      const mockReq = {
+        params: { quizId: expiredExam.quizId.toString() },
+        user: { _id: expiredExamUser._id },
+      };
+
+      await mockGetExamFeedback(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(200);
+      expect(examRes.body._id).to.equal(mockScore._id);
+      expect(examRes.body.user.toString()).to.equal(mockScore.user.toString());
+    });
+
+    it("should return a `User id is required` error message", async () => {
+      const mockReq = {
+        params: { quizId: expiredExam.quizId.toString() },
+        user: { _id: undefined },
+      };
+
+      await mockGetExamFeedback(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(403);
+      expect(examRes.body.message).to.equal(
+        "Error fetching exam feedback: User id is required"
+      );
+    });
+
+    it("should return a `Missing quiz id` error message", async () => {
+      const mockReq = {
+        params: { quizId: undefined },
+        user: { _id: expiredExamUser._id },
+      };
+
+      await mockGetExamFeedback(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(400);
+      expect(examRes.body.message).to.equal(
+        "Error fetching exam feedback: Missing quiz id"
+      );
+    });
+
+    it("should return a `Exam score not found` error message", async () => {
+      const mockReq = {
+        params: { quizId: expiredExam.quizId.toString() },
+        user: { _id: hasAnExamUser._id },
+      };
+
+      await mockGetExamFeedback(mockReq, examRes);
+
+      expect(examRes.statusCode).to.equal(404);
+      expect(examRes.body.message).to.equal(
+        "Error fetching exam feedback: Exam score not found"
+      );
     });
   });
 });
