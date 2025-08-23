7f93bf8ede6a1d33771a4421a8290c07100b6fc9
Martin <martin730036@gmail.com>
2025-08-23 23:41:27 +0200

    update: Socket new friend request unit test cases
    - integrate `handleNewRequest` and implement the test cases for successful, existing friend request, and invalid request data API calls.

--- test/unit/backend/socket/handleSocialUnit.test.js
+++ test/unit/backend/socket/handleSocialUnit.test.js
@@ -1,4 +1,7 @@
 import { expect } from "chai";
+import {
+  handleNewRequest,
+} from "../../../../backend/controller/socket/helpers/socket.friendController.js";
 import MockSocket from "../../../mocks/config/mockSocket.js";
 import MockData from "../../../mocks/config/mockData.js";
 import MockSocketModel from "../../../mocks/config/mockSocketModel.js";
@@ -86,4 +89,62 @@ describe("socket social controller API", () => {
   after(() => {
     mockUserModel.cleanupAll();
   });
+
+  describe("send friend request", () => {
+    it("should create a new friend request and verify it", async () => {
+      const reqSender = mockUsers[1];
+      const reqReceiver = mockUsers[2];
+      const eventData = {
+        senderId: reqSender._id,
+        receiverId: reqReceiver._id,
+      };
+
+      const response = await handleNewRequest(FriendFactory, eventData);
+
+      expect(response._id).to.equal(
+        `frdoc_${mockFriendModel.storage.friends.length}`
+      );
+      expect(response.senderId).to.equal(reqSender._id);
+      expect(response.senderName).to.equal(reqSender.name);
+      expect(response.senderAvatar).to.equal(reqSender.avatar);
+      expect(response.receiverId).to.equal(reqReceiver._id);
+      expect(response.receiverName).to.equal(reqReceiver.name);
+      expect(response.receiverAvatar).to.equal(reqReceiver.avatar);
+      expect(response.status).to.equal("pending");
+    });
+
+    it("should return a `Friend request already pending` error message", async () => {
+      const reqSender = mockUsers[2];
+      const reqReceiver = mockUsers[3];
+      const eventData = {
+        senderId: reqSender._id,
+        receiverId: reqReceiver._id,
+      };
+
+      try {
+        await handleNewRequest(FriendFactory, eventData);
+        throw new Error("Expected function to throw an error");
+      } catch (error) {
+        expect(error.message).to.equal("Friend request already pending");
+      }
+    });
+
+    it("should return a `Please provide valid friend request data` error message", async () => {
+      const reqReceiver = mockUsers[3];
+      const eventData = {
+        senderId: undefined,
+        receiverId: reqReceiver._id,
+      };
+
+      try {
+        await handleNewRequest(FriendFactory, eventData);
+        throw new Error("Expected function to throw an error");
+      } catch (error) {
+        expect(error.message).to.equal(
+          "Please provide valid friend request data"
+        );
+      }
+    });
+  });
+
 });
