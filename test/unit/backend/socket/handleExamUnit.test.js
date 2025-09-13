0b65b46942a9472c56a01a395379383a817a1d3f
Martin <martin730036@gmail.com>
2025-09-13 23:42:55 +0200

    update: Add `createExam` controller test suites
    - implement following test cases for `createExam` controller function: successful call, and erroneous calls - missing models, unauthorized access, unexistsing quiz and classroom query calls.

--- test/unit/backend/socket/handleExamUnit.test.js
+++ test/unit/backend/socket/handleExamUnit.test.js
@@ -48,3 +48,97 @@ describe("socket exam controllers", () => {
     userFactory.cleanupAll();
   });
 
+  describe("create exam", () => {
+    it("should create a new exam and verify it", async () => {
+      const models = {
+        Quiz: quizFactory,
+        Classroom: classroomFactory,
+        Exam: examFactory,
+      };
+      const eventData = { senderId: hasExamUser._id, quizId: createdQuiz._id };
+
+      const response = await createExam(models, eventData);
+
+      expect(response._id).to.equal("frdoc_1");
+      expect(response.quizId).to.equal(createdQuiz._id);
+      expect(response.studentId).to.equal(hasExamUser._id);
+      expect(response.studentId).to.equal(hasExamUser._id);
+      expect(response.examStart).to.not.be.null;
+
+      const examStart = new Date(response.examStart);
+      const examFinish = new Date(response.examFinish);
+
+      const timeDifferenceMs = examFinish - examStart;
+      const timeDifferenceMinutes = timeDifferenceMs / (1000 * 60);
+
+      expect(timeDifferenceMinutes).to.equal(quizTimeLimit);
+    });
+
+    it("should return a `Missing models` error message", async () => {
+      const models = {
+        Quiz: undefined,
+        Classroom: classroomFactory,
+        Exam: examFactory,
+      };
+
+      const eventData = { senderId: hasExamUser._id, quizId: createdQuiz._id };
+
+      try {
+        await createExam(models, eventData);
+      } catch (error) {
+        expect(error.message).to.equal("Missing models");
+      }
+    });
+    it("should return a `User not authorized` error message", async () => {
+      const models = {
+        Quiz: quizFactory,
+        Classroom: classroomFactory,
+        Exam: examFactory,
+      };
+
+      const eventData = { senderId: undefined, quizId: createdQuiz._id };
+
+      try {
+        await createExam(models, eventData);
+      } catch (error) {
+        expect(error.message).to.equal("User not authorized");
+      }
+    });
+
+    it("should return a `Quiz not found` error message", async () => {
+      const models = {
+        Quiz: quizFactory,
+        Classroom: classroomFactory,
+        Exam: examFactory,
+      };
+
+      const eventData = { senderId: hasExamUser._id, quizId: undefined };
+
+      try {
+        await createExam(models, eventData);
+      } catch (error) {
+        expect(error.message).to.equal("Quiz not found");
+      }
+    });
+
+    it("should return a `Classroom not found` error message", async () => {
+      const models = {
+        Quiz: quizFactory,
+        Classroom: classroomFactory,
+        Exam: examFactory,
+      };
+      const originalClassroom = classroomFactory.getStorage().classrooms;
+
+      await classroomFactory.deleteOne({ _id: originalClassroom._id });
+
+      const eventData = { senderId: noExamUser._id, quizId: createdQuiz._id };
+
+      try {
+        await createExam(models, eventData);
+      } catch (error) {
+        expect(error.message).to.equal("Classroom not found");
+      }
+
+      await classroomFactory.create(originalClassroom);
+    });
+
