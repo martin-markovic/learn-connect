2e627f0d8156dea791117a27f0dd988735b6c534
Martin <martin730036@gmail.com>
2025-09-22 22:37:18 +0200

    update: Enhance backend socket configuration
    - envelop the `configureSocket` function logic in `try...catch` block in `socketIo.js`.
    - envelop the `handleSocketLifeCycle` function logic in `try...catch` block, and add `context` validation and error handling in `manageSocketLifecycle.js`.

--- backend/config/socketIo.js
+++ backend/config/socketIo.js
@@ -2,9 +2,16 @@ import socketMiddleware from "../middleware/socketMiddleware.js";
 import handleSocketLifeCycle from "../controller/socket/managers/manageSocketLifecycle.js";
 
 const configureSocket = (io) => {
-  io.use(socketMiddleware);
+  try {
+    io.use(socketMiddleware);
 
-  handleSocketLifeCycle(io);
+    handleSocketLifeCycle(io);
+  } catch (error) {
+    console.error(
+      "Error configuring socket connection: ",
+      error.message || "Server error"
+    );
+  }
 };
 
 export default configureSocket;
