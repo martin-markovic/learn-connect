fcad0eeea0350880d3444018d9753a229a2fe1df
Martin <martin730036@gmail.com>
2025-10-08 12:34:35 +0200

    fix: Enhance classroom controller test suite
    - change response `success` property to default value of `true`, instead of depending on `matchedCount` property, to handle processing successful operations without unread messages in `updateChatMessages` in `socket.messageControllers.js`.
    - organize test cases into dedicated successful and invalid `describe` blocks, and
    move test cases into the respective `describe` blocks, in `classroomUnit.test.js`.

--- test/unit/backend/controller/classroomUnit.test.js
+++ test/unit/backend/controller/classroomUnit.test.js
@@ -49,199 +49,217 @@ describe("Classroom API", () => {
   });
 
   describe("joinClassroom", () => {
-    it("should register user in a classroom and verify it", async () => {
-      const mockReq = {
-        params: { classroomId: "1" },
-        user: { _id: firstClassroomUser._id },
-      };
+    describe("when payload is valid and complete should", () => {
+      it("should register user in a classroom and verify it", async () => {
+        const mockReq = {
+          params: { classroomId: "1" },
+          user: { _id: firstClassroomUser._id },
+        };
 
-      await mockJoinClassroom(mockReq, classroomRes);
+        await mockJoinClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(200);
-      expect(classroomRes.body.message).to.equal(
-        "User joined classroom successfully"
-      );
-      expect(classroomRes.body.updatedClassroom._id).to.equal(
-        firstClassroom._id
-      );
-      expect(classroomRes.body.updatedClassroom.name).to.equal(
-        firstClassroom.name
-      );
-      expect(classroomRes.body.updatedClassroom.subject).to.equal(
-        firstClassroom.subject
-      );
-      expect(classroomRes.body.updatedClassroom.limit).to.equal(
-        firstClassroom.limit
-      );
-      expect(classroomRes.body.updatedClassroom.students[0]).to.equal(
-        firstClassroomUser._id
-      );
+        expect(classroomRes.statusCode).to.equal(200);
+        expect(classroomRes.body.message).to.equal(
+          "User joined classroom successfully"
+        );
+        expect(classroomRes.body.updatedClassroom._id).to.equal(
+          firstClassroom._id
+        );
+        expect(classroomRes.body.updatedClassroom.name).to.equal(
+          firstClassroom.name
+        );
+        expect(classroomRes.body.updatedClassroom.subject).to.equal(
+          firstClassroom.subject
+        );
+        expect(classroomRes.body.updatedClassroom.limit).to.equal(
+          firstClassroom.limit
+        );
+        expect(classroomRes.body.updatedClassroom.students[0]).to.equal(
+          firstClassroomUser._id
+        );
+      });
     });
 
-    it("should return a `Classroom ID is required` error message", async () => {
-      const mockReq = {
-        params: { classroomId: undefined },
-        user: { _id: firstClassroomUser._id },
-      };
+    describe("when the request is invalid should", () => {
+      it("should return a `Classroom ID is required` error message", async () => {
+        const mockReq = {
+          params: { classroomId: undefined },
+          user: { _id: firstClassroomUser._id },
+        };
 
-      await mockJoinClassroom(mockReq, classroomRes);
+        await mockJoinClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(400);
-      expect(classroomRes.body.message).to.equal("Classroom ID is required");
-    });
+        expect(classroomRes.statusCode).to.equal(400);
+        expect(classroomRes.body.message).to.equal("Classroom ID is required");
+      });
 
-    it("should return a `User id is required` error message", async () => {
-      const mockReq = {
-        params: { classroomId: firstClassroom._id },
-        user: { _id: undefined },
-      };
+      it("should return a `User id is required` error message", async () => {
+        const mockReq = {
+          params: { classroomId: firstClassroom._id },
+          user: { _id: undefined },
+        };
 
-      await mockJoinClassroom(mockReq, classroomRes);
+        await mockJoinClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(403);
-      expect(classroomRes.body.message).to.equal("User id is required");
-    });
+        expect(classroomRes.statusCode).to.equal(403);
+        expect(classroomRes.body.message).to.equal("User id is required");
+      });
 
-    it("should return a `Classroom not found` error message", async () => {
-      const mockReq = {
-        params: { classroomId: "99" },
-        user: { _id: firstClassroomUser._id },
-      };
+      it("should return a `Classroom not found` error message", async () => {
+        const mockReq = {
+          params: { classroomId: "99" },
+          user: { _id: firstClassroomUser._id },
+        };
 
-      await mockJoinClassroom(mockReq, classroomRes);
+        await mockJoinClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(404);
-      expect(classroomRes.body.message).to.equal("Classroom not found");
-    });
+        expect(classroomRes.statusCode).to.equal(404);
+        expect(classroomRes.body.message).to.equal("Classroom not found");
+      });
 
-    it("should return a `User is already in this classroom` error message", async () => {
-      const mockReq = {
-        params: { classroomId: secondClassroom._id },
-        user: { _id: enrolledUser._id },
-      };
+      it("should return a `User is already in this classroom` error message", async () => {
+        const mockReq = {
+          params: { classroomId: secondClassroom._id },
+          user: { _id: enrolledUser._id },
+        };
 
-      await mockJoinClassroom(mockReq, classroomRes);
+        await mockJoinClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(400);
-      expect(classroomRes.body.message).to.equal(
-        "User is already in this classroom"
-      );
+        expect(classroomRes.statusCode).to.equal(400);
+        expect(classroomRes.body.message).to.equal(
+          "User is already in this classroom"
+        );
+      });
     });
   });
 
   describe("leaveClassroom", () => {
-    it("should leave a classroom and verify it", async () => {
-      const mockReq = {
-        params: { classroomId: secondClassroom._id },
-        user: { _id: enrolledUser._id },
-      };
+    describe("when payload is valid and complete should", () => {
+      it("should leave a classroom and verify it", async () => {
+        const mockReq = {
+          params: { classroomId: secondClassroom._id },
+          user: { _id: enrolledUser._id },
+        };
 
-      await mockLeaveClassroom(mockReq, classroomRes);
+        await mockLeaveClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(200);
-      expect(classroomRes.body).to.equal(secondClassroom._id);
+        expect(classroomRes.statusCode).to.equal(200);
+        expect(classroomRes.body).to.equal(secondClassroom._id);
+      });
     });
 
-    it("should return a `Please provide classroom ID` error message", async () => {
-      const mockReq = {
-        params: { classroomId: undefined },
-        user: { _id: enrolledUser._id },
-      };
+    describe("when the request is invalid should", () => {
+      it("should return a `Please provide classroom ID` error message", async () => {
+        const mockReq = {
+          params: { classroomId: undefined },
+          user: { _id: enrolledUser._id },
+        };
 
-      await mockLeaveClassroom(mockReq, classroomRes);
+        await mockLeaveClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(400);
-      expect(classroomRes.body.message).to.equal("Please provide classroom ID");
-    });
+        expect(classroomRes.statusCode).to.equal(400);
+        expect(classroomRes.body.message).to.equal(
+          "Please provide classroom ID"
+        );
+      });
 
-    it("should return a `User id is required` error message", async () => {
-      const mockReq = {
-        params: { classroomId: secondClassroom._id },
-        user: { _id: undefined },
-      };
+      it("should return a `User id is required` error message", async () => {
+        const mockReq = {
+          params: { classroomId: secondClassroom._id },
+          user: { _id: undefined },
+        };
 
-      await mockLeaveClassroom(mockReq, classroomRes);
+        await mockLeaveClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(403);
-      expect(classroomRes.body.message).to.equal("User id is required");
-    });
+        expect(classroomRes.statusCode).to.equal(403);
+        expect(classroomRes.body.message).to.equal("User id is required");
+      });
 
-    it("should return a `Classroom not found` error message", async () => {
-      const mockReq = {
-        params: { classroomId: "99" },
-        user: { _id: enrolledUser._id },
-      };
+      it("should return a `Classroom not found` error message", async () => {
+        const mockReq = {
+          params: { classroomId: "99" },
+          user: { _id: enrolledUser._id },
+        };
 
-      await mockLeaveClassroom(mockReq, classroomRes);
+        await mockLeaveClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(404);
-      expect(classroomRes.body.message).to.equal("Classroom not found");
-    });
+        expect(classroomRes.statusCode).to.equal(404);
+        expect(classroomRes.body.message).to.equal("Classroom not found");
+      });
 
-    it("should return a `You are not a member of this classroom` error message", async () => {
-      const mockReq = {
-        params: { classroomId: firstClassroom._id },
-        user: { _id: enrolledUser._id },
-      };
+      it("should return a `You are not a member of this classroom` error message", async () => {
+        const mockReq = {
+          params: { classroomId: firstClassroom._id },
+          user: { _id: enrolledUser._id },
+        };
 
-      await mockLeaveClassroom(mockReq, classroomRes);
+        await mockLeaveClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(403);
-      expect(classroomRes.body.message).to.equal(
-        "You are not a member of this classroom"
-      );
+        expect(classroomRes.statusCode).to.equal(403);
+        expect(classroomRes.body.message).to.equal(
+          "You are not a member of this classroom"
+        );
+      });
     });
   });
 
   describe("getAllClassrooms", () => {
-    it("should fetch list of all classrooms and verify it", async () => {
-      const mockReq = { user: { _id: firstClassroomUser._id } };
+    describe("when payload is valid and complete should", () => {
+      it("should fetch list of all classrooms and verify it", async () => {
+        const mockReq = { user: { _id: firstClassroomUser._id } };
 
-      await mockGetAllClassrooms(mockReq, classroomRes);
+        await mockGetAllClassrooms(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(200);
-      expect(classroomRes.body.length).to.equal(2);
-      expect(classroomRes.body[0]._id).to.equal(firstClassroom._id);
-      expect(classroomRes.body[1]._id).to.equal(secondClassroom._id);
+        expect(classroomRes.statusCode).to.equal(200);
+        expect(classroomRes.body.length).to.equal(2);
+        expect(classroomRes.body[0]._id).to.equal(firstClassroom._id);
+        expect(classroomRes.body[1]._id).to.equal(secondClassroom._id);
+      });
     });
 
-    it("should return a `User is not registered` error message", async () => {
-      const mockReq = { user: { _id: undefined } };
+    describe("when the request is invalid should", () => {
+      it("should return a `User is not registered` error message", async () => {
+        const mockReq = { user: { _id: undefined } };
 
-      await mockGetAllClassrooms(mockReq, classroomRes);
+        await mockGetAllClassrooms(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(403);
-      expect(classroomRes.body.message).to.equal("User is not registered");
+        expect(classroomRes.statusCode).to.equal(403);
+        expect(classroomRes.body.message).to.equal("User is not registered");
+      });
     });
   });
 
   describe("getUserClassrooms", () => {
-    it("should fetch a list of classroms and verify it", async () => {
-      const mockReq = { user: { _id: firstClassroomUser._id } };
+    describe("when payload is valid and complete should", () => {
+      it("should fetch a list of classroms and verify it", async () => {
+        const mockReq = { user: { _id: firstClassroomUser._id } };
 
-      await mockGetUserClassroom(mockReq, classroomRes);
+        await mockGetUserClassroom(mockReq, classroomRes);
 
-      for (const [key, value] of Object.entries(firstClassroom)) {
-        expect(classroomRes.body[key]).to.equal(value);
-      }
+        for (const [key, value] of Object.entries(firstClassroom)) {
+          expect(classroomRes.body[key]).to.equal(value);
+        }
+      });
+
+      it("should return null as a response payload and verify it", async () => {
+        const mockReq = { user: { _id: enrolledUser._id } };
+
+        await mockGetUserClassroom(mockReq, classroomRes);
+
+        expect(classroomRes.statusCode).to.equal(200);
+        expect(classroomRes.body).to.equal(null);
+      });
     });
 
-    it("should return null as a response payload and verify it", async () => {
-      const mockReq = { user: { _id: enrolledUser._id } };
+    describe("when the request is invalid should", () => {
+      it("should return a `User is not registered` error message", async () => {
+        const mockReq = { user: { _id: undefined } };
 
-      await mockGetUserClassroom(mockReq, classroomRes);
+        await mockGetUserClassroom(mockReq, classroomRes);
 
-      expect(classroomRes.statusCode).to.equal(200);
-      expect(classroomRes.body).to.equal(null);
-    });
-
-    it("should return a `User is not registered` error message", async () => {
-      const mockReq = { user: { _id: undefined } };
-
-      await mockGetUserClassroom(mockReq, classroomRes);
-
-      expect(classroomRes.statusCode).to.equal(403);
-      expect(classroomRes.body.message).to.equal("User is not registered");
+        expect(classroomRes.statusCode).to.equal(403);
+        expect(classroomRes.body.message).to.equal("User is not registered");
+      });
     });
   });
 });
