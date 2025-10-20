c72ea6cd49b23c479ed9ac0522b50f0dc01c5c48
Martin <martin730036@gmail.com>
2025-10-20 23:27:28 +0200

    update: Header.jsx logout test case and setup
    - integrate following dependencies: `fireEvent` from `@testing-library/react`, redux reducers for resetting user, quizzes, classroom, chat, exam and user list, and thunk for fetching exam data.
    - setup mock test functions for integrated reducers and thunks, using `jest.fn()`.
    - change `useNavigate` value to return `mockNavigate` function without calling it, to avoid returning undefined.
    - add new `describe` block for asserting `interactive behavior`, and add a new case `should dispatch reducers for resetting redux state on logout, and navigate to login page`.

--- frontend/src/__tests__/unit/components/headerUnit.test.js
+++ frontend/src/__tests__/unit/components/headerUnit.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-// } from "../../../features/quizzes/exam/examSlice.js";
+import { configureStore } from "@reduxjs/toolkit";
+import { resetUser } from "../../../features/auth/authSlice.js";
+import { resetQuizzes } from "../../../features/quizzes/quizSlice.js";
+import { resetClassroom } from "../../../features/classroom/classroomSlice.js";
+import { resetChat } from "../../../features/chat/chatSlice.js";
+import { resetUserList } from "../../../features/friend/friendSlice.js";
+import {
+  getExam,
+  resetExam,
+} from "../../../features/quizzes/exam/examSlice.js";
 import Header from "../../../components/Header.jsx";
 import { MemoryRouter } from "react-router-dom";
+
+jest.mock("../../../features/auth/authSlice.js", () => ({
+  resetUser: jest.fn(),
+}));
+
+jest.mock("../../../features/quizzes/quizSlice.js", () => ({
+  resetQuizzes: jest.fn(),
+}));
+
+jest.mock("../../../features/classroom/classroomSlice.js", () => ({
+  resetClassroom: jest.fn(),
+}));
+
+jest.mock("../../../features/chat/chatSlice.js", () => ({
+  resetChat: jest.fn(),
+}));
+
+jest.mock("../../../features/friend/friendSlice.js", () => ({
+  resetUserList: jest.fn(),
+}));
+
+jest.mock("../../../features/quizzes/exam/examSlice.js", () => ({
+  getExam: jest.fn(),
+  resetExam: jest.fn(),
+}));
+
+jest.mock("../../../hooks/useGlobalEvents.js", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 const mockNavigate = jest.fn();
 const mockDispatch = jest.fn();
@@ -10,7 +49,7 @@ const mockDispatch = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
 
-  useNavigate: () => mockNavigate(),
+  useNavigate: () => mockNavigate,
   useLocation: () => ({ pathname: "/" }),
 }));
 
@@ -121,7 +160,23 @@ describe("Header component", () => {
         expect(screen.queryByText(/Register/i)).toBeInTheDocument();
       });
     });
-    // describe("on unmount should", () => {});
   });
-  //   });
+
+  describe("interactive behavior", () => {
+    it("should navigate to auth pages on link clicks", () => {});
+
+    it("should dispatch reducers for resetting redux state on logout, and navigate to login page", () => {
+      renderWithStore(<Header />);
+
+      fireEvent.click(screen.getByText(/Logout/i));
+
+      expect(mockDispatch).toHaveBeenCalledWith(resetQuizzes());
+      expect(mockDispatch).toHaveBeenCalledWith(resetUser());
+      expect(mockDispatch).toHaveBeenCalledWith(resetChat());
+      expect(mockDispatch).toHaveBeenCalledWith(resetClassroom());
+      expect(mockDispatch).toHaveBeenCalledWith(resetUserList());
+      expect(mockDispatch).toHaveBeenCalledWith(resetExam());
+      expect(mockNavigate).toHaveBeenCalledWith("/login");
+    });
+  });
 });
